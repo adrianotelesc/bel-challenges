27f43fb5df561ecc30ab2cc72c3a1ea4d2d4da8e
adrianotelesc <adriano.telesc@gmail.com>
2020-11-08 08:43:47 -0400

    Fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
-    application
 }
-group = "me.adrianoteles"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
 dependencies {
     testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
-}
-application {
-    mainClassName = "MainKt"
 }
