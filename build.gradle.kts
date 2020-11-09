25499c7eb35afc4c9777a35a24e3a83089318400
adrianotelesc <adriano.telesc@gmail.com>
2020-11-09 18:03:16 -0400

    Add sonarqube plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("org.sonarqube") version "3.0"
 }
 
 repositories {
@@ -22,4 +23,12 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "adrianotelesc_bel-challenges")
+        property("sonar.organization", "adrianotelesc")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
