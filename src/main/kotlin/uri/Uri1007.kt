06e0bb0758e41c50061a3e1dcd7a20da02deb41f
Abeljunnior <abeljunnior@gmail.com>
2020-11-20 15:20:44 -0400

    Refatora a resolução Uri1007

--- src/main/kotlin/uri/Uri1007.kt
+++ src/main/kotlin/uri/Uri1007.kt
@@ -2,11 +2,6 @@ package uri
 
 object Uri1007 {
 
-    private val A = 0
-    private val B = 0
-    private val C = 0
-    private val D = 0
-
     @JvmStatic
     fun main(args: Array<String>? = null) {
         val a = readLine()?.toInt() ?: 0
