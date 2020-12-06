d901f45216ff520ca6814196d3fa8d723e1177c2
Abeljunnior <abeljunnior@gmail.com>
2020-12-06 11:41:21 -0400

    Alterar a nomenclatura das variáveis para English

--- src/main/kotlin/uri/Uri1008.kt
+++ src/main/kotlin/uri/Uri1008.kt
@@ -4,13 +4,13 @@ object Uri1008 {
 
     @JvmStatic
     fun main(args: Array<String>? = null) {
-        val numFunc = readLine()?.toInt() ?: 0
-        val qtdHoras = readLine()?.toInt() ?: 0
-        val valorHora = readLine()?.toDouble()?: 0.0
+        val employeeNumber = readLine()?.toInt() ?: 0
+        val workedHoursNumber = readLine()?.toInt() ?: 0
+        val amountPerHour = readLine()?.toDouble()?: 0.0
 
-        val salary = qtdHoras * valorHora
+        val salary = workedHoursNumber * amountPerHour
 
-        println("NUMBER = $numFunc")
+        println("NUMBER = $employeeNumber")
         println("SALARY = U$ %.2f".format(salary))
     }
 
