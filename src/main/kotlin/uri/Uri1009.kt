5379c206ed87246850092d4b5660f208f18255e9
Abeljunnior <abeljunnior@gmail.com>
2020-12-06 12:11:49 -0400

    Alterar a nomenclatura das variáveis para English

--- src/main/kotlin/uri/Uri1009.kt
+++ src/main/kotlin/uri/Uri1009.kt
@@ -5,12 +5,12 @@ object Uri1009 {
     @JvmStatic
     fun main(args: Array<String>? = null) {
         readLine()
-        val salario = readLine()!!.toDouble()
-        val totalVendas = readLine()!!.toDouble()
+        val salary = readLine()!!.toDouble()
+        val salesTotal = readLine()!!.toDouble()
 
-        val total = (salario + totalVendas * 0.15)
+        val totalSalary = (salary + salesTotal * 0.15)
 
-        println("TOTAL = R$ %.2f".format(total))
+        println("TOTAL = R$ %.2f".format(totalSalary))
     }
 
 }
